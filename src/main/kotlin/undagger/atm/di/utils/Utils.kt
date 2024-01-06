f6bc1de52167e0a2bf9731d66c7e11433354c857
Andrey Antipov <gorttar@gmail.com>
2024-01-07 02:39:03 +0400

    add OwnerlessReadOnlyProperty clarify KDoc

--- src/main/kotlin/undagger/atm/di/utils/Utils.kt
+++ src/main/kotlin/undagger/atm/di/utils/Utils.kt
@@ -1,41 +1,57 @@
 package undagger.atm.di.utils
 
 import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 /**
- * Create [new] bean of type [T] using its [constructor] and [this] bean import of type [D]
+ * Create [new] [Bean] using its [constructor] and [this] [Import]
  */
-inline fun <D, T> D.new(constructor: D.() -> T): T = constructor()
+inline fun <Import, Bean> Import.new(constructor: Import.() -> Bean): Bean = constructor()
 
 /**
- * Create [new] independent bean of type [T] using its [constructor]
+ * Create [new] independent [Bean] using its [constructor]
  */
-inline fun <T> new(constructor: () -> T): T = constructor()
+inline fun <Bean> new(constructor: () -> Bean): Bean = constructor()
 
 /**
- * Create [perRequest] delegate for bean of type [T] using its [constructor] and [this] bean import of type [D]
+ * Base interface that can be used for implementing owner and property independent (ownerless)
+ * property delegates of read-only properties.
+ *
+ * This is provided only for convenience; you don't have to extend this interface
+ * as long as your property delegate has methods with the same signatures.
+ *
+ * @param V the type of the property value.
+ * @see ReadOnlyProperty
+ */
+fun interface OwnerlessReadOnlyProperty<V> : ReadOnlyProperty<Any?, V> {
+    operator fun invoke(): V
+    override fun getValue(thisRef: Any?, property: KProperty<*>): V = this()
+}
+
+/**
+ * Create [perRequest] delegate for [Bean] using its [constructor] and [this] [Import]
  * Created delegate invokes [constructor] for each invocation of its [ReadOnlyProperty.getValue]
  */
-inline fun <D, T> D.perRequest(crossinline constructor: D.() -> T): ReadOnlyProperty<Any?, T> =
-    ReadOnlyProperty { _, _ -> constructor() }
+inline fun <Import, Bean> Import.perRequest(crossinline constructor: Import.() -> Bean): OwnerlessReadOnlyProperty<Bean> =
+    OwnerlessReadOnlyProperty { constructor() }
 
 /**
- * Create [perRequest] delegate for independent bean of type [T] using its [constructor]
+ * Create [perRequest] delegate for independent [Bean] using its [constructor]
  * Created delegate invokes [constructor] for each invocation of its [ReadOnlyProperty.getValue]
  */
-inline fun <T> perRequest(crossinline constructor: () -> T): ReadOnlyProperty<Any?, T> =
+inline fun <Bean> perRequest(crossinline constructor: () -> Bean): OwnerlessReadOnlyProperty<Bean> =
     Unit.perRequest { constructor() }
 
 /**
- * Create [perComponent] delegate for bean of type [T] using its [constructor] and [this] bean import of type [D]
+ * Create [perComponent] delegate [Bean] using its [constructor] and [this] [Import]
  * Created delegate invokes [constructor] once during first invocation of its [ReadOnlyProperty.getValue]
  */
-inline fun <T, D> D.perComponent(crossinline constructor: D.() -> T): ReadOnlyProperty<Any?, T> =
-    ReadOnlyProperty(lazy { constructor() }::getValue)
+inline fun <Import, Bean> Import.perComponent(crossinline constructor: Import.() -> Bean): OwnerlessReadOnlyProperty<Bean> =
+    OwnerlessReadOnlyProperty(lazy { constructor() }::value)
 
 /**
- * Create [perComponent] delegate for independent bean of type [T] using its [constructor]
+ * Create [perComponent] delegate for independent [Bean] using its [constructor]
  * Created delegate invokes [constructor] once during first invocation of its [ReadOnlyProperty.getValue]
  */
-inline fun <T> perComponent(crossinline constructor: () -> T): ReadOnlyProperty<Any?, T> =
+inline fun <Bean> perComponent(crossinline constructor: () -> Bean): OwnerlessReadOnlyProperty<Bean> =
     Unit.perComponent { constructor() }
