63d1333c021851ca03b0ed9280a6cea61747471d
Andrey Antipov <gorttar@gmail.com>
2024-01-07 17:33:07 +0400

    upgrade kotlin and dagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.22"
+    val kotlinVersion = "1.9.22"
+    kotlin("jvm") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
     application
-    kotlin("kapt") version "1.9.22"
 }
 
 group = "me.adp"
@@ -14,9 +15,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("com.google.dagger:dagger:2.50")
-    kapt("com.google.dagger:dagger-compiler:2.50")
+    testImplementation(kotlin("test", "1.9.22"))
+    val daggerVersion = "2.50"
+    implementation(group = "com.google.dagger", name = "dagger", version = daggerVersion)
+    kapt(group = "com.google.dagger", name = "dagger-compiler", version = daggerVersion)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 }
 
